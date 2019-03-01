7de03826fa6eeffe88fc25723c73d94a0e417f8c
d4rkdu0 <xssed@vip.qq.com>
2019-03-01 17:04:16 +0800

    update
    update

---
+++ js/vue.me.js
@@ -0,0 +1,136 @@
+new Vue({
+  el: "#app",
+  data: {
+    author: "d4rkdu0",
+    authorLink: "https://github.com/xssed",
+    musicText: "music",
+    musicUrl:
+      "https://www.xsser.cc/assets/canon.mp3",
+    copyright: "© 2019",
+    signature:
+      "欲买桂花同载酒，终不似，少年游。",
+    blogUrl: "https://www.xsser.cc",
+    avatarUrl:
+      "https://avatars0.githubusercontent.com/u/8002835",
+    blogText: "d4rkdu0",
+    leaveTitle: "点我点我",
+    returnTitle: "就很开心",
+    auth: "",
+    authed: false,
+    musicPlay: false,
+    publicLinks: [
+	  {
+        type: "lover",
+        link: "https://www.xsser.cc/about_me.html",
+        name: "lover",
+        iconClass: "fa fa-heart"
+      },
+	  {
+        type: "github",
+        link: "https://github.com/xssed",
+        name: "GitHub",
+        iconClass: "icon-github"
+      },
+      {
+        type: "mail",
+        link:
+          "http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&amp;amp;email=xsser@xsser.cc",
+        name: "MAIL",
+        iconClass: "icon-mail"
+      },
+      //{
+      //  type: "gogs",
+      //  link: "",
+      //  name: "gogs",
+      //  iconClass: "fa fa-code-fork"
+      //},
+      //{
+      //  type: "portainer-io",
+      //  link: "",
+      //  name: "portainer-io",
+      //  iconClass: "fa fa-codepen"
+      //}
+    ],
+    privateLinks: []
+  },
+  head: {
+    title: function() {
+      return {
+        inner: "d4rkdu0",
+        separator: "·"
+      };
+    },
+    meta: [
+      { name: "description", c: "Welcome", id: "desc" }
+    ],
+    link: [
+      {
+        rel: "icon",
+        href: "/favicon.png",
+        sizes: "16x16",
+        type: "image/x-icon"
+      }
+    ]
+  },
+  mounted() {
+    // this.checkMusicPlay();
+    // this.checkAuth();
+    var OriginTitile = document.title,
+      titleTime;
+    var app = this;
+    document.addEventListener("visibilitychange", function() {
+      if (document.hidden) {
+        document.title = app.leaveTitle;
+        clearTimeout(titleTime);
+      } else {
+        document.title = app.returnTitle;
+        titleTime = setTimeout(function() {
+          document.title = OriginTitile;
+        }, 2000);
+      }
+    });
+  },
+  methods: {
+    checkAuth() {
+      if (
+        !!localStorage.homepageAuthForCSTaoOrFriend &&
+        localStorage.homepageAuthForCSTaoOrFriend === this.auth
+      )
+        this.authed = true;
+      else this.authed = false;
+    },
+    lockClicked () {
+      this.checkAuth()
+      if (this.authed) localStorage.homepageAuthForCSTaoOrFriend = null;
+      else window.location = ''
+      this.checkAuth()
+    },
+    musicClicked () {
+      var audio = document.getElementById('bgm')
+      if (!this.musicPlay) {
+        localStorage.homepageMusicPlay = 1
+        this.musicPlay = true
+        if (audio !== null && audio.paused)
+          audio.play()
+      } else {
+        localStorage.homepageMusicPlay = 0
+        this.musicPlay = false
+        if (audio !== null && !audio.paused)
+          audio.pause()
+      }
+    },
+    checkMusicPlay () {
+      var audio = document.getElementById('bgm')
+      if (localStorage.homepageMusicPlay && localStorage.homepageMusicPlay == 1) {
+        this.musicPlay = true
+        if (audio !== null && audio.paused)
+          audio.play()
+      }
+      else {
+        this.musicPlay = false
+        if (audio !== null && !audio.paused)
+          audio.pause()
+      }
+    }
+  }
+});
